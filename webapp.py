30853f184bb4351f0cf1cc5cb1566d40f237c0eb
Azuthedog1 <sb.and.bigsur@gmail.com>
2021-03-16 10:18:39 -0700

    started

--- webapp.py
+++ webapp.py
@@ -4,14 +4,28 @@ import json
 
 app = Flask(__name__)
 
+def get_state_options(counties):
+	get_state_options = []
+	for s in counties:
+		s["County"]
+		if s in get_state_options:
+			print("in list")
+		else:
+			get_state_options.append(s)
+	for x in get_state_options:
+		y = y + Markup("<option value=\"" + x + "\">" + x + "</option>")
+	return y
+
 @app.route("/")
-def render_main():
-    return render_template('home.html')
-  
+def render_main(counties):
+		counties["County"]
+    return render_template('home.html', options=(get_state_options(counties)))
+
+
 @app.route("/response")
 def render_response():
-  
   return render_template()
 
+
 if __name__=="__main__":
-    app.run(debug=False, port=54321)
+    app.run(debug=True, port=54321)
