13e63d7d484dfa680f8d740f17015b3a73448e0a
Azuthedog1 <sb.and.bigsur@gmail.com>
2021-03-18 10:13:36 -0700

    worked on code

--- webapp.py
+++ webapp.py
@@ -7,29 +7,37 @@ app = Flask(__name__)
 def get_state_options(counties):
 	get_state_options = []
 	for s in counties:
-		s["County"]
-		if s in get_state_options:
-			print("in list")
-		else:
-			get_state_options.append(s)
+		if not(s['State']) in get_state_options:
+			get_state_options.append(s['State'])
+	y = ''
 	for x in get_state_options:
 		y = y + Markup("<option value=\"" + x + "\">" + x + "</option>")
+		print(y)
 	return y
 
 @app.route("/")
 def render_main():
 	with open('county_demographics.json') as demographics_data:
 		counties = json.load(demographics_data)
-	return render_template('home.html', options=(get_state_options(counties)))
+	return render_template('home.html', options = get_state_options(counties))
 
 
 @app.route("/response")
 def render_response():
-	fact = request.args['FunFact']
-	for data in conties:
-		if fact == ['State']:
-			print()
-	return render_template()
+	with open('county_demographics.json') as demographics_data:
+		counties = json.load(demographics_data)
+	fact = request.args['StateSelected']
+	fact1 = ""
+	for data in counties:
+		if fact == data["State"]:
+			first_county = counties[0]["Education"]["High School or Higher"]
+			for amount in counties:
+				amount["Education"]["High School or Higher"]
+				if amount["Education"]["High School or Higher"] > first_county:
+					print(first_county)
+					first_county = amount["Education"]["High School or Higher"]
+			fact1 = data["Education"]["High School or Higher"]
+	return render_template('response.html', response = fact1)
 
 
 if __name__=="__main__":
