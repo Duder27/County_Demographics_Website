9441a031fd95ce960637bf0b3fc03a5ea1f2dc98
Duder27 <77691123+Duder27@users.noreply.github.com>
2021-04-19 09:42:27 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -32,7 +32,7 @@ def render_main():
 	return render_template('home.html', options = get_state_options(counties), county_options = get_county_options(counties))
 
 
-@app.route("/response")
+@app.route("/response" method = 'GET')
 def render_response():
 	with open('county_demographics.json') as demographics_data:
 		counties = json.load(demographics_data)
